6217458486f5e6b606f0fc8297bae5e2e856e218
Santiago Córdova <dokho792@gmail.com>
2020-10-06 11:43:51 -0500

    CambiosPHP

---
+++ Conexion.php
@@ -0,0 +1,7 @@
+<?php
+if ($pdo = new PDO('pgsql:host=crudphp.c99qtfcpgajs.us-east-2.rds.amazonaws.com;dbname=PHPDeber1', 'postgres', '123456789')) {
+    echo "conectado";
+} else {
+    echo "la vrg";
+}
+?>
